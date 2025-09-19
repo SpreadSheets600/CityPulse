f9e81ec51e189972336df59aed028804dd7f3703
Soham Maity <sohammaity006@gmail.com>
2025-09-19 10:13:12 +0530

    feat: initial frontend commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="p-8 rounded-lg border-2">
+            <h1 class="text-4xl font-bold mb-4 text-center">VueJS Running</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style scoped></style>
