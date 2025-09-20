cd7842f9b425547712d8524413973da59a6138f0
Soham Maity <sohammaity006@gmail.com>
2025-09-20 15:29:07 +0530

    feat: update database URI to use SQLite and adjust JWT expiration to 7 days

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,19 +8,17 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "SuperSuperDuperSecretKey")
 
-    SQLALCHEMY_DATABASE_URI = os.getenv(
-        "DATABASE_URL", "postgresql://wsl:soham%40123@localhost:5432/city_pulse"
-    )
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///citypulse.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "AnotherSuperDuperSecretKey")
     JWT_TOKEN_LOCATION = ["headers", "cookies"]
-    JWT_ACCESS_TOKEN_EXPIRES = 36000
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=7)
 
     S3_CONFIG = {
         "endpoint_url": "https://us-003.s3.synologyc2.net",
-        "aws_access_key_id": "us2U1G1ii6Rt6tHwSapHstBZLqAlXF91",
-        "aws_secret_access_key": "wAPT0oKP3CD1E4SIEit5DvemLqpWbcmF",
+        "aws_access_key_id": "uspP7MHv7g1jNFePQNzMmVm3EPApqb91",
+        "aws_secret_access_key": "TmDOiMhLRQdVtJLm3Rw7G7DosbSh1Kmn",
         "bucket_name": "citypulse",
-        "region_name": "us-east-1",
+        "region_name": "us-003",
     }
