81c5151cc970625f729b6864215fa565658f410e
Soham Maity <sohammaity006@gmail.com>
2025-09-20 21:57:01 +0530

    feat: update model imports to include UserRole and IssueStatus for enhanced functionality

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -4,9 +4,10 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
-from .user import User
-from .issue import Issue
+from .user import User, UserRole
 from .department import Department
+from .issue import Issue, IssueStatus
+from .issue_update import IssueUpdate
 from .verification import VerificationStatus
 
 print("------ [ INFO ] ------ Models Imported Successfully")
