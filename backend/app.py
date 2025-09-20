c1e2dc75646f965d4b3989792ecdc8221c895822
Soham Maity <sohammaity006@gmail.com>
2025-09-20 15:29:01 +0530

    feat: refactor app initialization to use Flask-RESTful API and remove blueprints

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,13 @@
 import os
 from flask import Flask
 from flask_cors import CORS
+from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate, upgrade, stamp, init, migrate as alembic_migrate
 
 from api.models import db
-
-from api.routes.auth import auth_bp
-from api.routes.users import users_bp
-from api.routes.issues import issue_bp
+from api.routes.auth import Register, Login, Logout, Refresh
+from api.routes.issues import ReportIssue, MyIssues, GetIssue
 
 from config import Config
 
@@ -19,6 +18,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
     CORS(app)
 
     print("------ [ INFO ] ------ Initializing Database")
@@ -29,16 +29,33 @@ def create_app():
     print("------ [ INFO ] ------ Initializing JWT Manager")
     jwt = JWTManager(app)
 
-    print("------ [ INFO ] ------ Registering Blueprints")
-    app.register_blueprint(auth_bp, url_prefix="/api/auth")
-    app.register_blueprint(users_bp, url_prefix="/api/users")
-    app.register_blueprint(issue_bp, url_prefix="/api/issues")
+    print("------ [ INFO ] ------ Initializing Flask-RESTful API")
+    api = Api(app)
+
+    print("------ [ INFO ] ------ Registering Resources")
+    api.add_resource(Register, "/api/auth/register")
+    api.add_resource(Refresh, "/api/auth/refresh")
+    api.add_resource(Logout, "/api/auth/logout")
+    api.add_resource(Login, "/api/auth/login")
+
+    api.add_resource(ReportIssue, "/api/issues/report")
+    api.add_resource(GetIssue, "/api/issues/<int:issue_id>")
+
+    api.add_resource(MyIssues, "/api/issues/my-issues")
 
     @app.route("/ping", methods=["GET", "POST"])
     def ping():
         return "Pong!", 200
 
     return app
+
+
+def create_db():
+    if not os.path.exists("citypulse.db"):
+        print("------ [ INFO ] ------ Creating Database")
+        db.create_all()
+    else:
+        print("------ [ INFO ] ------ Database Already Exists")
 
 
 def run_migrations(app):
@@ -52,6 +69,8 @@ def run_migrations(app):
 
 if __name__ == "__main__":
     app = create_app()
-    run_migrations(app)
 
-    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
+    with app.app_context():
+        create_db()
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
