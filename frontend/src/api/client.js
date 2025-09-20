a1f044e797410be2fce46706442e47fedd9a5338
Soham Maity <sohammaity006@gmail.com>
2025-09-20 15:30:08 +0530

    refactor: remove default Content-Type header and unnecessary comment in axios response interceptor

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 axios.defaults.baseURL = 'http://localhost:5000'
-axios.defaults.headers.common['Content-Type'] = 'application/json'
 
 axios.interceptors.request.use(
   (config) => {
@@ -20,7 +19,6 @@ axios.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      // Token expired or invalid
       localStorage.removeItem('token')
       window.location.href = '/login'
     }
