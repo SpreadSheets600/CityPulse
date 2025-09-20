6aa927c251d69db4d52d2f2bd1cbc29e8e271686
Soham Maity <sohammaity006@gmail.com>
2025-09-20 21:58:15 +0530

    feat: add VueRouter plugin with logging and routes configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import VueRouter from 'unplugin-vue-router/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,19 @@ export default defineConfig({
     },
     allowedHosts: ['.ngrok-free.app'],
   },
-  plugins: [vue(), tailwindcss(), vueDevTools()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    vueDevTools(),
+    VueRouter({
+      logs: true,
+      routesFolder: [
+        {
+          src: 'src/views',
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
